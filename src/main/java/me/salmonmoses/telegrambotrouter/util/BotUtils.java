e9c244145ef4e9632a673146723093d359462c3e
Mikhail Melamed <mikhail.melamed@gmail.com>
2022-03-22 22:03:29 +0200

    Added getFromId() to BotUtils for getting user id from update

--- src/main/java/me/salmonmoses/telegrambotrouter/util/BotUtils.java
+++ src/main/java/me/salmonmoses/telegrambotrouter/util/BotUtils.java
@@ -39,4 +39,36 @@ public class BotUtils {
 			throw new IllegalStateException("Could not retrieve originating chat ID from update");
 		}
 	}
+
+	public static Long getFromId(Update update) {
+		if (MESSAGE.test(update)) {
+			return update.getMessage().getFrom().getId();
+		} else if (CALLBACK_QUERY.test(update)) {
+			return update.getCallbackQuery().getMessage().getFrom().getId();
+		} else if (INLINE_QUERY.test(update)) {
+			return update.getInlineQuery().getFrom().getId();
+		} else if (CHANNEL_POST.test(update)) {
+			return update.getChannelPost().getFrom().getId();
+		} else if (EDITED_CHANNEL_POST.test(update)) {
+			return update.getEditedChannelPost().getFrom().getId();
+		} else if (EDITED_MESSAGE.test(update)) {
+			return update.getEditedMessage().getFrom().getId();
+		} else if (CHOSEN_INLINE_QUERY.test(update)) {
+			return update.getChosenInlineQuery().getFrom().getId();
+		} else if (SHIPPING_QUERY.test(update)) {
+			return update.getShippingQuery().getFrom().getId();
+		} else if (PRECHECKOUT_QUERY.test(update)) {
+			return update.getPreCheckoutQuery().getFrom().getId();
+		} else if (POLL_ANSWER.test(update)) {
+			return update.getPollAnswer().getUser().getId();
+		} else if (POLL.test(update)) {
+			return EMPTY_USER.getId();
+		} else if (MY_CHAT_MEMBER.test(update)) {
+			return update.getMyChatMember().getFrom().getId();
+		} else if (CHAT_MEMBER.test(update)) {
+			return update.getChatMember().getFrom().getId();
+		} else {
+			throw new IllegalStateException("Could not retrieve originating chat ID from update");
+		}
+	}
 }
